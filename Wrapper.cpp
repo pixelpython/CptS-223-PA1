c516f296d8c4918cea54c16de9227637064ba744
Alec Barran <alexanderbarran@gmail.com>
2022-09-15 18:44:34 -0700

    Complete assignment requirements
    * implement writing to files
    
    * complete assignment requirements

--- Wrapper.cpp
+++ Wrapper.cpp
@@ -1,21 +1,5 @@
 #include "Wrapper.h"
 
-bool Wrapper::checkOpenFiles() {
-	// check that commands file opened successfully
-	if (!commandsStream.is_open()) {
-		cout << "Unable to open " << mCommandsFile;
-		return false;
-	}
-
-	// similarly check previous games file
-	if (!gameStream.is_open()) {
-		cout << "Unable to open " << mGameFile;
-		return false;
-	}
-
-	return true;
-}
-
 /*************************************************************
  * Function: run ()                                          *
  * Description: runs the program wrapper, opening files and  *
@@ -23,7 +7,7 @@ bool Wrapper::checkOpenFiles() {
  *************************************************************/
 void Wrapper::run() {
 	int option, score = 0;
-	bool exit = !checkOpenFiles();
+	bool exit = false;
 
     // import initial list of commands from csv
     importCmdList();
@@ -58,8 +42,9 @@ void Wrapper::run() {
 		case 6:
 			exit = true;
 			if (this->modified) {
-				saveFiles();
+				saveCmds();
 			}
+			saveProfiles();
 			break;
 		}
 	}
@@ -84,7 +69,7 @@ void Wrapper::printMenu() {
  * Description: print game rules to the screen               *
  *************************************************************/
 void Wrapper::printRules() {
-	cout << "The objective of the game is to match Linux commands to appropriate descriptions of those commands. If a command is matched, then the player earns 1 point. The if the command is not matched, then the player loses a point. Yes, negative point totals are possible. The player selects the number of match questions at the beginning of the game. The game continue until the number is reached.";
+	cout << "The objective is to match Linux commands to descriptions of those commands. If a command guess, the player earns 1 point. If the guess is incorrect, the player loses a point. Negative point totals are possible. The player selects between five and thirty questions at the beginning of the game.";
 	cout << endl;
 }
 
@@ -98,15 +83,21 @@ void Wrapper::importCmdList() {
 	mCmds.clearList();
 	cmdData newData;
 
+	// open command file for reading
+	std::ifstream cmdStream;
+	cmdStream.open(mCommandsFile);
+
 	// with help from https://stackoverflow.com/questions/5343173/
-	commandsStream.clear();
-	commandsStream.seekg(0);
+	cmdStream.clear();
+	cmdStream.seekg(0);
 	
 	// iterate over each line of file
-	while (getline(commandsStream, line)) {
+	while (getline(cmdStream, line)) {
 		newData = parseCmdLine(line);
 		mCmds.insertAtFront(newData);
 	}
+
+	cmdStream.close();
 }
 
 /*************************************************************
@@ -116,11 +107,16 @@ void Wrapper::importCmdList() {
  *************************************************************/
 void Wrapper::importProfiles() {
 	string line;
+	// clear player profile array
 	for (int i = 0; i < 16; i++) {
 		players[i].name = "";
 		players[i].score = 0;
 	}
 	playerCount = 0;
+
+	// open player file for reading
+	std::ifstream gameStream;
+	gameStream.open(mGameFile);
 
 	// with help from https://stackoverflow.com/questions/5343173/
 	gameStream.clear();
@@ -131,6 +127,8 @@ void Wrapper::importProfiles() {
 		players[playerCount] = parseProfile(line);
 		playerCount++;
 	}
+
+	gameStream.close();
 }
 
 /*************************************************************
@@ -196,12 +194,18 @@ profile Wrapper::parseProfile(string line) {
 void Wrapper::playGame(int startScore) {
 	int score = startScore;
 	int totalGuesses = 0;
+	string name;
+	// prompt user for name
+	cout << "What is your name? ";
+	cin >> name;
+
 	// prompt user for questions score to end game
 	int questions = promptIntInRange(5, 30, "How many questions will you answer? ");
 	int cmdList[3];
 	int printOrder[3];
 	int usedCmds[mCmds.getLength()] = {0};
 
+	// game loop
 	for (int i = 0; i < questions; i++) {
 		// get three random commands
 		do {
@@ -243,6 +247,9 @@ void Wrapper::playGame(int startScore) {
 			cout << mCmds.getNodeAtPosition(0)->data.name << " - " << mCmds.getNodeAtPosition(0)->data.description << endl;
 		}
 	}
+
+	// save score to user profile
+	updateProfile(name, score);
 }
 
 /*************************************************************
@@ -277,13 +284,41 @@ void Wrapper::loadPreviousGame(int &score) {
  * Description: allows user to add command to quiz list      *
  *************************************************************/
 void Wrapper::addCmd() {
-	string name, description;
-	cout << "Enter the command name to add: ";
-	cin >> name;
-	cout << "Enter the command description: ";
-	cin >> description;
-	cout << "Command '" << name << "' successfully added." << endl;
+	cmdData newData;
+	bool valid = false;
+	node<cmdData>* pCurrent;
 
+	while (!valid) {
+		cout << "Enter the command name to add [or 'quit']: ";
+		cin >> newData.name;
+
+		valid = true;
+		pCurrent = mCmds.getHead();
+		// ensure command isn't a duplicate
+		while (pCurrent != nullptr) {
+			// allow user to quit
+			if (newData.name == "quit") {
+				return;
+			}
+			if (pCurrent->data.name == newData.name) {
+				valid = false;
+				break;
+			}
+			pCurrent = pCurrent->next;
+		}
+
+		if (!valid) {
+			cout << "Duplicate command entered. Please try again." << endl;
+		}
+	}
+	
+	cout << "Enter the command description: ";
+	// ingest newline in first
+	getline(cin, newData.description);
+	getline(cin, newData.description);
+	cout << "Command '" << newData.name << "' successfully added." << endl;
+
+	mCmds.insertAtFront(newData);
 	this->modified = true;
 }
 
@@ -307,6 +342,7 @@ void Wrapper::removeCmd() {
 				removed = mCmds.removeNode(pCurrent);
 				break;
 			}
+			pCurrent = pCurrent->next;
 		}
 
 		// inform user whether command was successfully removed
@@ -323,13 +359,61 @@ void Wrapper::removeCmd() {
 	}
 }
 
+void Wrapper::updateProfile(string name, int score) {
+	// see if name matches in existing array
+	for (int i = 0; i < playerCount; i++) {
+		// match found, update score
+		if (players[i].name == name) {
+			players[i].score = score;
+			return;
+		}
+	}
+
+	// else, need to create new profile
+	if (playerCount < 16) {
+		players[playerCount].name = name;
+		players[playerCount].score = score;
+		playerCount++;
+	} else {
+		// player array is out of memory
+		cout << "Player profile array is full. Score will not be saved." << endl;
+	}
+}
+
 /*************************************************************
- * Function: saveFiles ()                                    *
- * Description: saves game and command data to scores.csv    *
- *        and commands.csv, respectively                     *
+ * Function: saveCmds ()                                     *
+ * Description: saves command data to commands.csv           *
  *************************************************************/
-void Wrapper::saveFiles() {
-	
+void Wrapper::saveCmds() {
+	// open commands file for writing, and overwrite data
+	std::ofstream cmdStream;
+	cmdStream.open(mCommandsFile, std::ofstream::trunc);
+
+	// iterate over command linked list and print each entry to file
+	node<cmdData>* pCurrent = mCmds.getHead();
+	while (pCurrent != nullptr) {
+		cmdStream << pCurrent->data.name << ",\"" << pCurrent->data.description << "\"\n";
+		pCurrent = pCurrent->next;
+	}
+
+	cmdStream.close();
+}
+
+/*************************************************************
+ * Function: saveProfiles ()                                 *
+ * Description: saves game data to scores.csv                *
+ *************************************************************/
+void Wrapper::saveProfiles() {
+	// open games file for writing, and overwrite data
+	std::ofstream gameStream;
+	gameStream.open(mGameFile);
+
+	// iterate over player profiles array and print each to file
+	for (int i = 0; i < playerCount; i++) {
+		gameStream << players[i].name << "," << players[i].score << "\n";
+	}
+
+	gameStream.close();
 }
 
 /*************************************************************
